fb72a4a6af890845aa1245c9bef3c2f5ba6ded7b
nirmal-bluefunda <168703116+nirmal-bluefunda@users.noreply.github.com>
2026-06-24 17:22:57 +0530

    fix: updated text color in the apple signin button (#67)

--- CAI/Views/ContentView.swift
+++ CAI/Views/ContentView.swift
@@ -590,7 +590,7 @@ private struct SocialSignInButton<Icon: View>: View {
     }
 
     private var backgroundColor: Color {
-        style == .filled ? .primary : Color(.systemBackground)
+        style == .filled ? .black : Color(.systemBackground)
     }
 
     private var foregroundColor: Color {
@@ -598,7 +598,7 @@ private struct SocialSignInButton<Icon: View>: View {
     }
 
     private var borderColor: Color {
-        style == .filled ? .clear : Color(.systemGray4)
+        Color(.systemGray4)
     }
 
     var body: some View {
